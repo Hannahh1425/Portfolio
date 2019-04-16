8fecfdf787176b51b42837699da4ba1742c41677
Hannahh1425 <yuw431@ucsd.edu>
2019-04-16 13:46:39 -0700

    Updates

---
+++ precache-manifest.eb3a0694e03422677f6fa73d4eb31029.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "679ed70f3a495415fe6902147e0c9792",
+    "url": "/portfolio/static/media/capitalsphere2.679ed70f.png"
+  },
+  {
+    "revision": "88bbcb9118ba7729d0a2",
+    "url": "/portfolio/static/css/main.2d90ee22.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "bced3fc6ba467460f97c",
+    "url": "/portfolio/static/js/2.bced3fc6.chunk.js"
+  },
+  {
+    "revision": "688dda2556e74daee70c192c3305c098",
+    "url": "/portfolio/static/media/iphone.688dda25.png"
+  },
+  {
+    "revision": "b12ec6a0540d415f861a293c94f21704",
+    "url": "/portfolio/static/media/icon-task.b12ec6a0.png"
+  },
+  {
+    "revision": "8aaa56c551be4bca9c6c1ad753401769",
+    "url": "/portfolio/static/media/desktop1.8aaa56c5.png"
+  },
+  {
+    "revision": "a2460c2417ae34fc47fa8538aa63ff82",
+    "url": "/portfolio/static/media/capitalsphere1.a2460c24.png"
+  },
+  {
+    "revision": "ec8da4034f2ba87dbff65ceed2f587c4",
+    "url": "/portfolio/static/media/familyhub_demo.ec8da403.jpg"
+  },
+  {
+    "revision": "45e50d0b3b7f1d7fc5d8cb75fad431ff",
+    "url": "/portfolio/static/media/medalarm3.45e50d0b.jpg"
+  },
+  {
+    "revision": "1a8e5d04cc3f8b9c90dfa697d0ce7335",
+    "url": "/portfolio/static/media/med-alarm-full.1a8e5d04.png"
+  },
+  {
+    "revision": "db986dc387c7dc5833f8eae83fd704b3",
+    "url": "/portfolio/static/media/family-hub-full.db986dc3.png"
+  },
+  {
+    "revision": "07f7df68673a8ae59334cc72d93a295c",
+    "url": "/portfolio/static/media/capitalsphere-full.07f7df68.png"
+  },
+  {
+    "revision": "52ccf78e8976b5d73fe75149a73f0860",
+    "url": "/portfolio/static/media/lito-nail-full.52ccf78e.png"
+  },
+  {
+    "revision": "029903a1c622c3a05836cd23fadf5aae",
+    "url": "/portfolio/static/media/me.029903a1.jpg"
+  },
+  {
+    "revision": "d8513599dc8ca435f5320b32e856ea73",
+    "url": "/portfolio/static/media/icon-education.d8513599.png"
+  },
+  {
+    "revision": "ff1b50c23e60f0e8da2516a8975d052d",
+    "url": "/portfolio/static/media/icon-goal.ff1b50c2.png"
+  },
+  {
+    "revision": "ac06f1f00f7c9a6f9f923296c08092c1",
+    "url": "/portfolio/static/media/Resume.ac06f1f0.pdf"
+  },
+  {
+    "revision": "88bbcb9118ba7729d0a2",
+    "url": "/portfolio/static/js/main.88bbcb91.chunk.js"
+  },
+  {
+    "revision": "4a500374646eedefb4fc64b16ce51ae8",
+    "url": "/portfolio/static/media/familyhub1.4a500374.jpg"
+  },
+  {
+    "revision": "2d858039b466adcd962d6de151797b1b",
+    "url": "/portfolio/static/media/medalarm1.2d858039.jpg"
+  },
+  {
+    "revision": "23ea2fc71ede25c36a5b44784848a5fe",
+    "url": "/portfolio/static/media/medalarm2.23ea2fc7.jpg"
+  },
+  {
+    "revision": "39fc3723110a288be48411bc4eb3d96e",
+    "url": "/portfolio/static/media/medalarm4.39fc3723.jpg"
+  },
+  {
+    "revision": "75fd147fae451f128af9ebb3b7d8b14c",
+    "url": "/portfolio/static/media/medalarm5.75fd147f.jpg"
+  },
+  {
+    "revision": "f06180b0cadcd60f6f164388a7a4a04c",
+    "url": "/portfolio/static/media/medalarm6.f06180b0.jpg"
+  },
+  {
+    "revision": "94943880d76fc26f28e90ee8b0a04dad",
+    "url": "/portfolio/static/media/medalarm7.94943880.jpg"
+  },
+  {
+    "revision": "f283c4fac3326b7c76b8f15190e65913",
+    "url": "/portfolio/static/media/medalarm8.f283c4fa.jpg"
+  },
+  {
+    "revision": "195a528ace36ed1d69f1e539ec3e1e92",
+    "url": "/portfolio/static/media/lito-demo.195a528a.png"
+  },
+  {
+    "revision": "77227a18e9b2d9744cdd2b0673f2341f",
+    "url": "/portfolio/static/media/lito1.77227a18.png"
+  },
+  {
+    "revision": "b65aaebb745fc7e8a7cf949006b9aecb",
+    "url": "/portfolio/static/media/lito2.b65aaebb.png"
+  },
+  {
+    "revision": "48b2f2d76fdc68904a4437f96e290811",
+    "url": "/portfolio/static/media/lito3.48b2f2d7.png"
+  },
+  {
+    "revision": "daa34016a9704eec8694b8183705d024",
+    "url": "/portfolio/static/media/litonail-low.daa34016.jpg"
+  },
+  {
+    "revision": "fb32e67bd16831f0437e3f197f5a20f0",
+    "url": "/portfolio/static/media/icon-progress.fb32e67b.png"
+  },
+  {
+    "revision": "168d806b13dbcebe2579947a01334225",
+    "url": "/portfolio/static/media/lito4.168d806b.png"
+  },
+  {
+    "revision": "cbafaac7ab806b92248498a810148e69",
+    "url": "/portfolio/static/media/lito5.cbafaac7.png"
+  },
+  {
+    "revision": "f1d1654798466b6b7a64af61f4732338",
+    "url": "/portfolio/index.html"
+  }
+];
