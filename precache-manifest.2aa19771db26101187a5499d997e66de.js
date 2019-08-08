f4d8ba7c1cb9cf097c4c05a89b2412dccd07c57e
Hannahh1425 <yuw431@ucsd.edu>
2019-08-07 22:59:51 -0700

    Updates

---
+++ precache-manifest.2aa19771db26101187a5499d997e66de.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b517b6f8b45d7165bfdff08e826d00",
+    "url": "/portfolio/static/media/d4sd_home4.b8b517b6.png"
+  },
+  {
+    "revision": "217bd01355718b007302",
+    "url": "/portfolio/static/css/main.80e0ae34.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "1807428ef0d41ee1f73f",
+    "url": "/portfolio/static/js/2.1807428e.chunk.js"
+  },
+  {
+    "revision": "b12ec6a0540d415f861a293c94f21704",
+    "url": "/portfolio/static/media/icon-task.b12ec6a0.png"
+  },
+  {
+    "revision": "688dda2556e74daee70c192c3305c098",
+    "url": "/portfolio/static/media/iphone.688dda25.png"
+  },
+  {
+    "revision": "8aaa56c551be4bca9c6c1ad753401769",
+    "url": "/portfolio/static/media/desktop1.8aaa56c5.png"
+  },
+  {
+    "revision": "ec8da4034f2ba87dbff65ceed2f587c4",
+    "url": "/portfolio/static/media/familyhub_demo.ec8da403.jpg"
+  },
+  {
+    "revision": "45e50d0b3b7f1d7fc5d8cb75fad431ff",
+    "url": "/portfolio/static/media/medalarm3.45e50d0b.jpg"
+  },
+  {
+    "revision": "e37af594bbfbfb72257d4daa72335fe6",
+    "url": "/portfolio/static/media/lito_final1.e37af594.png"
+  },
+  {
+    "revision": "b278d9da1d872c855a5785be052788dc",
+    "url": "/portfolio/static/media/lito_final2.b278d9da.png"
+  },
+  {
+    "revision": "9095120f20f54184f74ec23d77f4b3ff",
+    "url": "/portfolio/static/media/Capitalsphere_desktop.9095120f.png"
+  },
+  {
+    "revision": "9e74520ef18490d45ec5196c6856293b",
+    "url": "/portfolio/static/media/d4sd.9e74520e.png"
+  },
+  {
+    "revision": "57bb249200b76ad135f916e57ee1d00a",
+    "url": "/portfolio/static/media/lito-nail.57bb2492.png"
+  },
+  {
+    "revision": "29e2f94af5023d649fa4d381f7a222e6",
+    "url": "/portfolio/static/media/4032.29e2f94a.png"
+  },
+  {
+    "revision": "56a2f4f5198d1454832f2f9a61832167",
+    "url": "/portfolio/static/media/skill_figma.56a2f4f5.png"
+  },
+  {
+    "revision": "ec861b4827617230c5e22e06cece7037",
+    "url": "/portfolio/static/media/skill_ps.ec861b48.png"
+  },
+  {
+    "revision": "775ff7023673eae8865e1960e6709cc2",
+    "url": "/portfolio/static/media/skill_xd.775ff702.png"
+  },
+  {
+    "revision": "f36145c63391461c48b57b9aefe46f43",
+    "url": "/portfolio/static/media/skill_react.f36145c6.png"
+  },
+  {
+    "revision": "0f887ccb667e648c9ec2862d8484aa2a",
+    "url": "/portfolio/static/media/skill_java.0f887ccb.png"
+  },
+  {
+    "revision": "38fde3b7aa20d5681c846e4d0360e748",
+    "url": "/portfolio/static/media/skill_html.38fde3b7.png"
+  },
+  {
+    "revision": "6f08d8673708d99bbb53ca175e6d8ead",
+    "url": "/portfolio/static/media/skill_js.6f08d867.png"
+  },
+  {
+    "revision": "e152f9e2065a15378307155ead80647f",
+    "url": "/portfolio/static/media/skill_css.e152f9e2.png"
+  },
+  {
+    "revision": "07fd359efbd00af297c4f60fbbff2e3e",
+    "url": "/portfolio/static/media/skill_prototyping.07fd359e.png"
+  },
+  {
+    "revision": "3df22e968a536879698af682c064057b",
+    "url": "/portfolio/static/media/skill_storyboarding.3df22e96.png"
+  },
+  {
+    "revision": "9fc3d5637c909daf2a18d41dc6e013fa",
+    "url": "/portfolio/static/media/skill_usertesting.9fc3d563.png"
+  },
+  {
+    "revision": "4a500374646eedefb4fc64b16ce51ae8",
+    "url": "/portfolio/static/media/familyhub1.4a500374.jpg"
+  },
+  {
+    "revision": "176a142f7401d0e80b2e6a443722bea7",
+    "url": "/portfolio/static/media/d4sd_iter11.176a142f.png"
+  },
+  {
+    "revision": "663e25d03d44faca6cffbeb311ee24af",
+    "url": "/portfolio/static/media/d4sd_iter12.663e25d0.png"
+  },
+  {
+    "revision": "c40f6f439851a8754cfa63ac9dec6a0a",
+    "url": "/portfolio/static/media/d4sd_iter13.c40f6f43.png"
+  },
+  {
+    "revision": "c08a179673bf54946eee932bc963a0f1",
+    "url": "/portfolio/static/media/d4sd_iter14.c08a1796.png"
+  },
+  {
+    "revision": "543e0257dfe8ad40df0817df3d47ac96",
+    "url": "/portfolio/static/media/d4sd_about1.543e0257.png"
+  },
+  {
+    "revision": "5159b87b0546cee5c958df5bc97e334d",
+    "url": "/portfolio/static/media/d4sd_about2.5159b87b.png"
+  },
+  {
+    "revision": "c3206366bb2293656a58b7bb2a464322",
+    "url": "/portfolio/static/media/d4sd_about3.c3206366.jpg"
+  },
+  {
+    "revision": "d3b580102e51935446347f1f5ede53aa",
+    "url": "/portfolio/static/media/d4sd_home1.d3b58010.png"
+  },
+  {
+    "revision": "f8612bca21d5878139c96a962601c455",
+    "url": "/portfolio/static/media/d4sd_home2.f8612bca.png"
+  },
+  {
+    "revision": "f33aa4308c8258ffbb9cc74eca43828f",
+    "url": "/portfolio/static/media/d4sd_home3.f33aa430.png"
+  },
+  {
+    "revision": "217bd01355718b007302",
+    "url": "/portfolio/static/js/main.217bd013.chunk.js"
+  },
+  {
+    "revision": "aebb49e77418e2075a16a5adc7178aed",
+    "url": "/portfolio/static/media/d4sd_home5.aebb49e7.png"
+  },
+  {
+    "revision": "245c2094cfda5205bfc5a622a6796375",
+    "url": "/portfolio/static/media/d4sd_home6.245c2094.png"
+  },
+  {
+    "revision": "2d858039b466adcd962d6de151797b1b",
+    "url": "/portfolio/static/media/medalarm1.2d858039.jpg"
+  },
+  {
+    "revision": "23ea2fc71ede25c36a5b44784848a5fe",
+    "url": "/portfolio/static/media/medalarm2.23ea2fc7.jpg"
+  },
+  {
+    "revision": "39fc3723110a288be48411bc4eb3d96e",
+    "url": "/portfolio/static/media/medalarm4.39fc3723.jpg"
+  },
+  {
+    "revision": "75fd147fae451f128af9ebb3b7d8b14c",
+    "url": "/portfolio/static/media/medalarm5.75fd147f.jpg"
+  },
+  {
+    "revision": "f06180b0cadcd60f6f164388a7a4a04c",
+    "url": "/portfolio/static/media/medalarm6.f06180b0.jpg"
+  },
+  {
+    "revision": "94943880d76fc26f28e90ee8b0a04dad",
+    "url": "/portfolio/static/media/medalarm7.94943880.jpg"
+  },
+  {
+    "revision": "f283c4fac3326b7c76b8f15190e65913",
+    "url": "/portfolio/static/media/medalarm8.f283c4fa.jpg"
+  },
+  {
+    "revision": "9c17ad5b6cfda61bc6309571dceed9f3",
+    "url": "/portfolio/static/media/litonail_skew.9c17ad5b.gif"
+  },
+  {
+    "revision": "a84cf861851e31b58a286cb0bc04bdf6",
+    "url": "/portfolio/static/media/lito_moodboard.a84cf861.jpg"
+  },
+  {
+    "revision": "9f36701793d5de0cd1039570b8fb1c63",
+    "url": "/portfolio/static/media/lito_comp.9f367017.png"
+  },
+  {
+    "revision": "daa34016a9704eec8694b8183705d024",
+    "url": "/portfolio/static/media/lito_paper_pro.daa34016.jpg"
+  },
+  {
+    "revision": "46e3e044e9d08d709fd92880699af8c3",
+    "url": "/portfolio/static/media/lito_iter1.46e3e044.png"
+  },
+  {
+    "revision": "6b38616711ff0624a7b5e317eb5d360d",
+    "url": "/portfolio/static/media/lito_iter2.6b386167.png"
+  },
+  {
+    "revision": "5578163ea8c0d6ed4e9e957c440ca018",
+    "url": "/portfolio/static/media/lito_review1.5578163e.png"
+  },
+  {
+    "revision": "5d05fd2dea5cf381774883b0cd4c18e7",
+    "url": "/portfolio/static/media/lito_nail.5d05fd2d.png"
+  },
+  {
+    "revision": "fab30f8cf89f94fdbc93e6bb122e2773",
+    "url": "/portfolio/static/media/lito_eye.fab30f8c.png"
+  },
+  {
+    "revision": "b932621e58d135a7133f7383041c1647",
+    "url": "/portfolio/static/media/lito_waxing.b932621e.png"
+  },
+  {
+    "revision": "14638598ccc2cb54f8e17f758244a309",
+    "url": "/portfolio/static/media/lito_about.14638598.png"
+  },
+  {
+    "revision": "c35fa5af93acb8e81c83001feb4ddb49",
+    "url": "/portfolio/static/media/lito_contact.c35fa5af.png"
+  },
+  {
+    "revision": "88299080da60c739d51f584b4d27e437",
+    "url": "/portfolio/static/media/lito_lookbook.88299080.png"
+  },
+  {
+    "revision": "e5404ba6e49489e851789df156acb12e",
+    "url": "/portfolio/static/media/lito_ia.e5404ba6.png"
+  },
+  {
+    "revision": "ac06f1f00f7c9a6f9f923296c08092c1",
+    "url": "/portfolio/static/media/Resume.ac06f1f0.pdf"
+  },
+  {
+    "revision": "4047335fc3bec106180d0fd456c870ed",
+    "url": "/portfolio/static/media/med-alarm.4047335f.png"
+  },
+  {
+    "revision": "db986dc387c7dc5833f8eae83fd704b3",
+    "url": "/portfolio/static/media/family-hub-full.db986dc3.png"
+  },
+  {
+    "revision": "cf92dac7c8fd9d9887ca36eee3aabdef",
+    "url": "/portfolio/static/media/d4sd_platform.cf92dac7.png"
+  },
+  {
+    "revision": "ffcf34c21403b780143df1c202330a06",
+    "url": "/portfolio/static/media/about_food.ffcf34c2.png"
+  },
+  {
+    "revision": "a2460c2417ae34fc47fa8538aa63ff82",
+    "url": "/portfolio/static/media/capitalsphere1.a2460c24.png"
+  },
+  {
+    "revision": "679ed70f3a495415fe6902147e0c9792",
+    "url": "/portfolio/static/media/capitalsphere2.679ed70f.png"
+  },
+  {
+    "revision": "72382af5d70190b91f89a932f13cae35",
+    "url": "/portfolio/static/media/d4sd_style1.72382af5.jpg"
+  },
+  {
+    "revision": "fe231e12c207a943ffb94f02309f95f1",
+    "url": "/portfolio/static/media/d4sd_style2.fe231e12.jpg"
+  },
+  {
+    "revision": "f181ee8a82cc4d59a27bbd53e394917c",
+    "url": "/portfolio/static/media/d4sd_style3.f181ee8a.jpg"
+  },
+  {
+    "revision": "758e22edb858e0e7dffa4b328b31fb49",
+    "url": "/portfolio/static/media/d4sd_iter2.758e22ed.png"
+  },
+  {
+    "revision": "41044a9e4e172ba1efbb92acc3c76625",
+    "url": "/portfolio/static/media/lito_review2.41044a9e.png"
+  },
+  {
+    "revision": "58c20b72291d4a1a3a20af338a639d4e",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.29539ba46fc4a0cb1913b7706fd939c0.js"
+  "/portfolio/precache-manifest.2aa19771db26101187a5499d997e66de.js"
 );
 
 workbox.clientsClaim();
